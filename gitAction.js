bed582f48f783b5fb0225f97000a9a162a2d46b2
Rki0 <pky00823@gmail.com>
2025-02-12 09:21:03 +0900

    💄 Style: insert line-break character

--- gitAction.js
+++ gitAction.js
@@ -3,6 +3,7 @@ import path from "path";
 
 export const blockDirectCommitCommand = () => {
   const huskyPreCommitPath = path.resolve(process.cwd(), ".husky/pre-commit");
+
   const gitCommitBlockCode = `if [ "$CZ_TEST" != "true" ]; then
   echo "Don't use git commit. Please use npm run commit."
   exit 1
